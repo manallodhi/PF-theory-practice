249b73f07574ca54f94a796116dbc74f7e3dc317
manallodhi <lodhimanal8@gmail.com>
2025-01-10 21:54:41 +0500

    Added

---
+++ Simple Pattern.cpp
@@ -0,0 +1,21 @@
+// using nested while loop for printing pattern .
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int i = 1 ;
+   while(i<=5)
+   {
+     int j = 1 ;
+     while(j<=i)
+     {
+      cout << i ;
+      j++;
+     }
+    cout<<endl;
+    i++;
+  }
+  return 0;
+}
